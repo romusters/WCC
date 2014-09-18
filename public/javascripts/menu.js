6dcd7d144f5d73f78430e2fd7a0622388ea2ed68
semitom <tom.gankema@gmail.com>
2014-09-18 19:00:13 +0200

    reactive buttons added comments

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -2,23 +2,24 @@
  * Created by tom on 17-9-14.
  */
 $(document).ready(function() {
+    /* activate correct nav items at load of page */
     $(".nav").find("li:first").addClass("active");
 
+    /* change activated nav items on click */
     $(".menu").find("li").on("click", function(){
         $(".menu .active").removeClass("active");
-        /*$(this).addClass("active");*/
-        /*$(".menu").find("li").eq($(this).index()).addClass("active");*/
         var n = $(this).index()+1;
         $(".menu").find("li:nth-child(" + n + ")").addClass("active");
     });
 
+    /* change text on click of nav-tab */
     $(".nav-tabs").find("li").on("click", function(){
         $(".nav-tabs .active").removeClass("active");
         $(this).addClass("active");
         $(".namechange").text($(this).find("a").text());
     });
 
-    /*
+    /* example ajax post; how does this work exactly?
     $.ajax({
         url: jsRoutes.controllers.Application.previousRecordings().url,
         type: "POST",
