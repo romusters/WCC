e52d4d1cb37374ff2a9d26e4706db4e5245f4f32
semitom <tom.gankema@gmail.com>
2014-09-18 15:06:24 +0200

    reactive buttons

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -3,4 +3,30 @@
  */
 $(document).ready(function() {
     $(".nav").find("li:first").addClass("active");
+
+    $(".menu").find("li").on("click", function(){
+        $(".menu .active").removeClass("active");
+        $(this).addClass("active");
+        $(".menu").find("li").eq($(this).index()).addClass("active");
+    });
+
+    $(".nav-tabs").find("li").on("click", function(){
+        $(".nav-tabs .active").removeClass("active");
+        $(this).addClass("active");
+        $(".namechange").text($(this).find("a").text());
+    });
+
+    /*
+    $.ajax({
+        url: jsRoutes.controllers.Application.previousRecordings().url,
+        type: "POST",
+        dataType: "json",
+        data: { questions: "Why is there air?" }
+    }).done(function(childNodes) {
+        alert("TODO do something")
+    }).fail(function(jqXHR, textStatus, errorThrown){
+        alert(textStatus + ", " + errorThrown);
+    });
+    */
+
 });
